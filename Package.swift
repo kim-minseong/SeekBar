f87d72b98b066efff2fd97db43c84eccc84faafe
Minseong Kim <alwaysineeedyou@gmail.com>
2024-07-06 16:25:48 +0900

    Temporarily add support for the platform

--- Package.swift
+++ Package.swift
@@ -5,10 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "SeekBar",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SeekBar",
+            type: .dynamic,
             targets: ["SeekBar"]),
     ],
     targets: [

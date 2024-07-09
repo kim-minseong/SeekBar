c6edfdfeeaad0cf0a610a58774059a2ec5048bc3
Minseong Kim <alwaysineeedyou@gmail.com>
2024-07-09 21:01:52 +0900

    Prepared project for uimplementation of sample app showcasing library use cases

---
+++ SeekBarSamples/SeekBarSamples/SeekBarSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeekBarSamplesApp.swift
+//  SeekBarSamples
+//
+//  Created by Minseong Kim on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct SeekBarSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

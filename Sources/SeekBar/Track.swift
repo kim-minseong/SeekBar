520c8c15e0567c758e131258065e592d6e39a30f
Minseong Kim <alwaysineeedyou@gmail.com>
2024-07-12 23:30:38 +0900

    Temporarily implemented the segment feature in Track

--- Sources/SeekBar/Track.swift
+++ Sources/SeekBar/Track.swift
@@ -10,6 +10,8 @@ struct Track: View {
     var bufferedValue: CGFloat?
     let bounds: ClosedRange<CGFloat>
     
+    var timelineSegments: [TimelineSegmentPoint] = []
+    
     // TODO: Encapsulate properties related to appearance.
     let activeTrackColor: Color = .accentColor
     let inactiveTrackColor: Color = .gray.opacity(0.3)
@@ -22,17 +24,32 @@ struct Track: View {
     var body: some View {
         Canvas { context, size in
             // Inactive track
-            context.drawTrack(size: size, color: inactiveTrackColor, width: size.width, trackHeight: trackHeight, cornerRadius: cornerRadius)
+            context.drawLine(size: size, color: inactiveTrackColor, width: size.width, height: trackHeight, cornerRadius: cornerRadius)
             
             // Buffer track
             if let bufferedValue {
                 let bufferedWidth = width(for: bufferedValue, in: bounds, with: size.width)
-                context.drawTrack(size: size, color: bufferedTrackColor, width: bufferedWidth, trackHeight: trackHeight, cornerRadius: cornerRadius)
+                context.drawLine(size: size, color: bufferedTrackColor, width: bufferedWidth, height: trackHeight, cornerRadius: cornerRadius)
             }
             
             // Active track
             let activeWidth = width(for: value, in: bounds, with: size.width)
-            context.drawTrack(size: size, color: activeTrackColor, width: activeWidth, trackHeight: trackHeight, cornerRadius: cornerRadius)
+            context.drawLine(size: size, color: activeTrackColor, width: activeWidth, height: trackHeight, cornerRadius: cornerRadius)
+            
+            // Draws a gap between timeline segments if any exist.
+            if !timelineSegments.isEmpty {
+                var segmentContext = context
+                segmentContext.blendMode = .clear
+                
+                for segment in timelineSegments {
+                    segmentContext.drawGap(
+                        size: size,
+                        width: gapWidth,
+                        height: trackHeight,
+                        segment: segment
+                    )
+                }
+            }
         }
         .drawingGroup()
     }
@@ -50,28 +67,53 @@ struct Track: View {
     }
 }
 
-// MARK: Canvas Context Extenstion
+// MARK: - Canvas Context Extenstion
 
 extension GraphicsContext {
-    /// Draws a track with the specified size, color, and track dimensions.
+    /// Draws a line with the specified size, color, and dimensions.
     ///
     /// - Parameters:
     ///   - size: The size of the canvas.
-    ///   - color: The color of the track.
-    ///   - width: The width of the track.
-    ///   - trackHeight: The height of the track.
-    ///   - cornerRadius: The corner radius of the track.
-    func drawTrack(size: CGSize, color: Color, width: CGFloat, trackHeight: CGFloat, cornerRadius: CGFloat) {
-        let trackRect = CGRect(
+    ///   - color: The color of the line.
+    ///   - width: The width of the line.
+    ///   - height: The height of the line.
+    ///   - cornerRadius: The corner radius of the line (optional).
+    func drawLine(size: CGSize, color: Color, width: CGFloat, height: CGFloat, cornerRadius: CGFloat? = nil) {
+        let lineRect = CGRect(
             x: 0,
-            y: (size.height - trackHeight) / 2,
+            y: (size.height - height) / 2,
             width: width,
-            height: trackHeight
+            height: height
         )
-        let trackPath = Path { path in
-            path.addRoundedRect(in: trackRect, cornerSize: CGSize(width: cornerRadius, height: cornerRadius))
+        let linePath = Path { path in
+            if let cornerRadius {
+                // Add rounded path when cornderRadius value is exists.
+                path.addRoundedRect(in: lineRect, cornerSize: CGSize(width: cornerRadius, height: cornerRadius))
+            } else {
+                path.addRect(lineRect)
+            }
         }
-        self.fill(trackPath, with: .color(color))
+        self.fill(linePath, with: .color(color))
+    }
+    
+    /// Draws a gap at the specified segment position.
+    ///
+    /// - Parameters:
+    ///   - size: The size of the canvas.
+    ///   - width: The width of the gap.
+    ///   - height: The height of the gap.
+    ///   - segment: The segment representing the position of the gap.
+    func drawGap(size: CGSize, width: CGFloat, height: CGFloat, segment: TimelineSegmentPoint) {
+        let segmentRect = CGRect(
+            x: size.width * segment.endPoint,
+            y: (size.height - height) / 2,
+            width: width,
+            height: height
+        )
+        let segmentPath = Path { path in
+            path.addRect(segmentRect)
+        }
+        self.fill(segmentPath, with: .color(.clear))
     }
 }
 
@@ -80,6 +122,13 @@ extension GraphicsContext {
 struct Track_Previews: PreviewProvider {
     static let trackWithBufferPreviewName = "Track With Buffer Preview"
     static let defaultTrackPreviewName = "Default Track Preview"
+    static let trackWithTimelinePreviewName = "Track With Timeline Preview"
+    
+    static let timelineSegments = [
+        TimelineSegmentPoint(name: "Timeline 1", startPoint: 0.0, endPoint: 0.2),
+        TimelineSegmentPoint(name: "Timeline 2", startPoint: 0.2, endPoint: 0.6),
+        TimelineSegmentPoint(name: "Timeline 3", startPoint: 0.6, endPoint: 0.8)
+    ]
     
     static var previews: some View {
         Group {
@@ -93,6 +142,12 @@ struct Track_Previews: PreviewProvider {
                 .frame(height: 4)
                 .padding()
                 .previewDisplayName(trackWithBufferPreviewName)
+                .previewLayout(.sizeThatFits)
+            
+            Track(value: 0.3, bounds: 0.0...1.0, timelineSegments: timelineSegments)
+                .frame(height: 4)
+                .padding()
+                .previewDisplayName(trackWithTimelinePreviewName)
                 .previewLayout(.sizeThatFits)
         }
     }
